d17832c24a652432de1109ed25a9e0c4df538fa6
Scott Merritt <smerritt987@gmail.com>
2020-03-22 13:55:19 -0500

    added a simple set up script with some helpful aliases in bash

---
+++ battleship/setup.sh
@@ -0,0 +1,5 @@
+alias RUN_BATTLESHIP="mvn exec:java -Dexec.mainClass=\"battleship.Game\""
+
+alias clean_package="mvn clean install && mvn clean compile && mvn package"
+
+alias run_tests="mvn test"
