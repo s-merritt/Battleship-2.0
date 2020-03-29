a51fad01e48f6bdb110666ccbac2f7859a58a968
s-merritt <smerritt987@gmail.com>
2020-03-29 15:55:22 -0500

    created unit tests for exceptions

---
+++ battleship/src/test/java/exceptions/TestLocationAlreadyGuessedException.java
@@ -0,0 +1,51 @@
+package exceptions;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import exceptions.TestLocationAlreadyGuessedException;
+
+/**
+ * Unit tests objects.Coordinate
+ */
+public class TestLocationAlreadyGuessedException extends TestCase {
+
+  /**
+   * Create the test case
+   * 
+   * @param testname name of the test case
+   */
+  public TestLocationAlreadyGuessedException(String testname) {
+    super(testname);
+  }
+
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite(){
+    return new TestSuite(TestLocationAlreadyGuessedException.class);
+  }
+
+  public void testConstructor(){
+    LocationAlreadyGuessedException e = new LocationAlreadyGuessedException();
+    assertNotNull(e);
+  }
+
+  public void testMessage(){
+    LocationAlreadyGuessedException e = new LocationAlreadyGuessedException("this is a test message");
+    assertEquals("this is a test message", e.getLocalizedMessage());
+  }
+
+  public void testCause(){
+    LocationAlreadyGuessedException e = new LocationAlreadyGuessedException(new Throwable());
+    assertNotNull(e.getCause());
+  }
+
+  public void testCauseAndMessage(){
+    LocationAlreadyGuessedException e = new LocationAlreadyGuessedException("test message", new Throwable());
+    assertEquals("test message", e.getLocalizedMessage());
+    assertNotNull(e.getCause());
+  }
+
+}
